43209900ef84ac80c89896fb18cecdcd4c4555e1
飯田周平 <iidashuhei@shuhei.local>
2020-03-20 10:15:15 +0900

    登録できた

--- src/main/java/com/example/controller/ArticleRegisterController.java
+++ src/main/java/com/example/controller/ArticleRegisterController.java
@@ -19,7 +19,6 @@ import com.example.domain.Article;
 import com.example.domain.Ramen;
 import com.example.domain.RamenImage;
 import com.example.domain.RamenShop;
-import com.example.domain.RamenShopTime;
 import com.example.form.ArticleRegisterForm;
 import com.example.repository.ArticleRepository;
 import com.example.repository.RamenImageRepository;
@@ -80,32 +79,11 @@ public class ArticleRegisterController {
 
 	@RequestMapping("/insert")
 	public String insert(@Validated ArticleRegisterForm articleRegisterForm, BindingResult result, Model model) throws IOException {
-		RamenShop ramenShop = new RamenShop();
-		ramenShop.setShopName(articleRegisterForm.getShopName());
-		ramenShop.setZipcode(articleRegisterForm.getZipcode());
-		ramenShop.setPrefecture(articleRegisterForm.getPrefecture());
-		ramenShop.setCity(articleRegisterForm.getCity());
-		ramenShop.setOther(articleRegisterForm.getOther());
-		ramenShop.setHolidays(articleRegisterForm.getHolidays());
-		ramenShop.setCreatedBy("飯田");
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		ramenShop.setCreatedAt(timestamp);
-		ramenShopRepository.insert(ramenShop);
-
+	
 		Article article = new Article();
 		article.setUserId(1);
-		article.setShopId(ramenShop.getShopId());
+		article.setShopId(articleRegisterForm.getShopId());
 		articleRepository.insert(article);
-
-		RamenShopTime ramenShopTime = new RamenShopTime();
-		ramenShopTime.setShopId(ramenShop.getShopId());
-		ramenShopTime.setDaysId(articleRegisterForm.getDaysId());
-		ramenShopTime.setNoonStartTime(articleRegisterForm.getNoonStartTime());
-		ramenShopTime.setNoonEndTime(articleRegisterForm.getNoonEndTime());
-		ramenShopTime.setNightStartTime(articleRegisterForm.getNightStartTime());
-		ramenShopTime.setNightEndTime(articleRegisterForm.getNightEndTime());
-		ramenShopTime.setOtherTime(articleRegisterForm.getOtherTime());
-		ramenShopTimeRepository.insert(ramenShopTime);
 
 		// 画像ファイル形式チェック
 		MultipartFile image = articleRegisterForm.getRamenImage();
@@ -135,11 +113,12 @@ public class ArticleRegisterController {
 		ramenImageRepository.insert(ramenImage);
 		
 		Ramen ramen = new Ramen();
-		ramen.setShopId(ramenShop.getShopId());
+		ramen.setShopId(articleRegisterForm.getShopId());
 		ramen.setRamenName(articleRegisterForm.getRamenName());
 		ramen.setRamenPrice(articleRegisterForm.getRamenPrice());
 		ramen.setRamenImagePathId(ramenImage.getImageId());
 		ramen.setCreatedBy("飯田");
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 		ramen.setCreatedAt(timestamp);
 		ramenRepository.insert(ramen);
 		return "redirect:/";
--- src/main/java/com/example/form/ArticleRegisterForm.java
+++ src/main/java/com/example/form/ArticleRegisterForm.java
@@ -1,6 +1,5 @@
 package com.example.form;
 
-import java.sql.Timestamp;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,24 +8,6 @@ public class ArticleRegisterForm {
 	// Article
 	private Integer userId;
 	private Integer shopId;
-
-	// RamenShop
-	private String shopName;
-	private String zipcode;
-	private String prefecture;
-	private String city;
-	private String other;
-	private String holidays;
-	private String createdBy;
-	private Timestamp createdAt;
-
-	// RamenShopTime
-	private Integer daysId;
-	private String noonStartTime;
-	private String noonEndTime;
-	private String nightStartTime;
-	private String nightEndTime;
-	private String otherTime;
 
 	// Ramen
 	private String ramenName;
@@ -50,118 +31,6 @@ public class ArticleRegisterForm {
 
 	public void setShopId(Integer shopId) {
 		this.shopId = shopId;
-	}
-
-	public String getShopName() {
-		return shopName;
-	}
-
-	public void setShopName(String shopName) {
-		this.shopName = shopName;
-	}
-
-	public String getZipcode() {
-		return zipcode;
-	}
-
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
-
-	public String getPrefecture() {
-		return prefecture;
-	}
-
-	public void setPrefecture(String prefecture) {
-		this.prefecture = prefecture;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getOther() {
-		return other;
-	}
-
-	public void setOther(String other) {
-		this.other = other;
-	}
-
-	public String getHolidays() {
-		return holidays;
-	}
-
-	public void setHolidays(String holidays) {
-		this.holidays = holidays;
-	}
-
-	public String getCreatedBy() {
-		return createdBy;
-	}
-
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
-
-	public Timestamp getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Timestamp createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public Integer getDaysId() {
-		return daysId;
-	}
-
-	public void setDaysId(Integer daysId) {
-		this.daysId = daysId;
-	}
-
-	public String getNoonStartTime() {
-		return noonStartTime;
-	}
-
-	public void setNoonStartTime(String noonStartTime) {
-		this.noonStartTime = noonStartTime;
-	}
-
-	public String getNoonEndTime() {
-		return noonEndTime;
-	}
-
-	public void setNoonEndTime(String noonEndTime) {
-		this.noonEndTime = noonEndTime;
-	}
-
-	public String getNightStartTime() {
-		return nightStartTime;
-	}
-
-	public void setNightStartTime(String nightStartTime) {
-		this.nightStartTime = nightStartTime;
-	}
-
-	public String getNightEndTime() {
-		return nightEndTime;
-	}
-
-	public void setNightEndTime(String nightEndTime) {
-		this.nightEndTime = nightEndTime;
-	}
-
-	public String getOtherTime() {
-		return otherTime;
-	}
-
-	public void setOtherTime(String otherTime) {
-		this.otherTime = otherTime;
 	}
 
 	public String getRamenName() {
@@ -198,13 +67,9 @@ public class ArticleRegisterForm {
 
 	@Override
 	public String toString() {
-		return "ArticleRegisterForm [userId=" + userId + ", shopId=" + shopId + ", shopName=" + shopName + ", zipcode="
-				+ zipcode + ", prefecture=" + prefecture + ", city=" + city + ", other=" + other + ", holidays="
-				+ holidays + ", createdBy=" + createdBy + ", createdAt=" + createdAt + ", daysId=" + daysId
-				+ ", noonStartTime=" + noonStartTime + ", noonEndTime=" + noonEndTime + ", nightStartTime="
-				+ nightStartTime + ", nightEndTime=" + nightEndTime + ", otherTime=" + otherTime + ", ramenName="
-				+ ramenName + ", ramenPrice=" + ramenPrice + ", ramenImagePathId=" + ramenImagePathId + ", ramenImage="
-				+ ramenImage + "]";
+		return "ArticleRegisterForm [userId=" + userId + ", shopId=" + shopId + ", ramenName=" + ramenName
+				+ ", ramenPrice=" + ramenPrice + ", ramenImagePathId=" + ramenImagePathId + ", ramenImage=" + ramenImage
+				+ "]";
 	}
 
 }
