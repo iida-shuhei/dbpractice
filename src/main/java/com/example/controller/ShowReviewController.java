8e0cd6b9d7040ae905dec3ada398ac8ec94b439e
飯田周平 <iidashuhei@shuhei.local>
2020-04-22 20:16:39 +0900

    hey

--- src/main/java/com/example/controller/ShowReviewController.java
+++ src/main/java/com/example/controller/ShowReviewController.java
@@ -83,7 +83,6 @@ public class ShowReviewController {
 		
 		User user = userService.findByUserId(loginUser.getUser().getUserId());
 		model.addAttribute("user", user);
-		model.addAttribute("message","投稿されたラーメン記事");
 		return "review_list";
 	}
 	
